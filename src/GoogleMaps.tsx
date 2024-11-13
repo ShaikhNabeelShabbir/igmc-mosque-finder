a1c6d4d9cfd1e93bd1215128a8208f776d918bdb
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-11-13 12:38:38 +0530

    added tenth basic

---
+++ src/GoogleMaps.tsx
@@ -0,0 +1,12 @@
+//goolemaps
+const GoogleMaps = () => {
+  return (
+    <div>
+      <div className="mt-5 w-full h-96 border border-purple-600">
+        GoogleMaps
+      </div>
+    </div>
+  );
+};
+
+export default GoogleMaps;
